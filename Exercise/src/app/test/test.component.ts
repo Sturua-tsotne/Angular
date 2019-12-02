d31a6ee05e650e630be3d6d7bcc27a706eb48695
Sturua-tsotne <47318145+Sturua-tsotne@users.noreply.github.com>
2019-12-02 13:48:02 +0400

    სიტყვების დამუშავება;  ათწილადების  კონტროლი,; პროცენტები;  ვალუტა; დრო

---
+++ Exercise/src/app/test/test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+  public name="Tsotne";
+  public message="welcome to tsotne";
+  public={
+    "firstname":"Tsotne",
+    "lastname":"sturua"
+  }
+
+public date= new Date();
+
+   ngOnInit() {
+  }
+
+
+
+
+
+
+}
